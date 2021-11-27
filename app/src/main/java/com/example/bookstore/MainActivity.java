835ab9f55fb5a164cf9cdb0bfb20453da88cc701
19127495 <64051739+19127495@users.noreply.github.com>
2021-11-28 00:34:29 +0700

    buyer's homepage, chua thiet ke category

--- app/src/main/java/com/example/bookstore/MainActivity.java
+++ app/src/main/java/com/example/bookstore/MainActivity.java
@@ -1,14 +1,36 @@
 package com.example.bookstore;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import androidx.appcompat.widget.Toolbar;
+
+import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
-
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
+    Toolbar toolBar;
+    @SuppressLint("CutPasteId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.login_screen);
+        setContentView(R.layout.homepage_buyer);
+
+        drawerLayout = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.nav_view);
+        toolBar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolBar);
+
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolBar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
     }
+
+
+
 }
