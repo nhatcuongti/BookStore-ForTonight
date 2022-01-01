e8d4b029c41025800f1d265a96c5f0a5f27ae687
lthieu288 <letrunghieu2882001@gmail.com>
2022-01-02 03:13:45 +0700

    kết nối firebase của màn hình đăng ký vs đăng nhập

--- app/src/main/java/com/example/bookstore/LoginActivity.java
+++ app/src/main/java/com/example/bookstore/LoginActivity.java
@@ -2,25 +2,93 @@ package com.example.bookstore;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.bookstore.fragment.HomePageFragment;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
 public class LoginActivity extends AppCompatActivity {
     TextView register;
+    EditText username, password;
+    Button btn_login;
+    private static FirebaseFirestore db = FirebaseFirestore.getInstance();
+    private static final String TAG = "Cloud Firestore";
+
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_screen);
 
         //cài đặt textview đăng ký
         register = (TextView) findViewById(R.id.dangky);
+        username = (EditText)findViewById(R.id.login_username);
+        password = (EditText) findViewById(R.id.login_password);
+        btn_login = (Button)findViewById(R.id.login_btn);
+
+        btn_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG, "Username:" + username.getText().toString());
+                Log.d(TAG, "Password: " + password.getText().toString());
+                if(username.getText().toString().equals("") || password.getText().toString().equals("")){
+                    Toast.makeText(LoginActivity.this, "Bạn cần phải điền đầy đủ thông tin", Toast.LENGTH_SHORT).show();
+                }
+                else{
+                    db.collection("users")
+                            .get()
+                            .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                                @Override
+                                public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                                    if (task.isSuccessful()) {
+                                        boolean have = false;
+                                        for (QueryDocumentSnapshot document : task.getResult()) {
+                                            if(username.getText().toString().equals(document.getId())){
+                                                if(password.getText().toString().equals(document.get("password").toString())){
+                                                    have = true;
+                                                    if(document.get("role").toString().equals("Người mua hàng")){
+                                                        Intent intent = new Intent(getApplicationContext(), MainBuyerHomepageActivity.class);
+                                                        startActivity(intent);
+                                                    }
+                                                    else {
+                                                        Intent intent = new Intent(getApplicationContext(), MainBuyerHomepageActivity.class);
+                                                        startActivity(intent);
+                                                    }
+                                                    break;
+                                                }
+                                            }
+//                                            Log.d(TAG, document.getId() + " => " + document.getData());
+                                        }
+                                        if(!have)
+                                            Toast.makeText(LoginActivity.this, "Sai mật khẩu hoặc tài khoản", Toast.LENGTH_SHORT).show();
+
+
+                                    } else {
+                                        Log.w(TAG, "Error getting documents.", task.getException());
+                                    }
+                                }
+                            });
+                }
+
+            }
+        });
         register.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
-//                Toast.makeText(LoginActivity.this, "Testing", Toast.LENGTH_SHORT).show();
+                Toast.makeText(LoginActivity.this, "Testing", Toast.LENGTH_SHORT).show();
+
                 Intent intent = new Intent(getApplicationContext(), RegisterActivity.class);
                 startActivity(intent);
 
--- app/src/main/java/com/example/bookstore/RegisterActivity.java
+++ app/src/main/java/com/example/bookstore/RegisterActivity.java
@@ -13,8 +13,14 @@ import android.widget.Toast;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-public class RegisterActivity extends AppCompatActivity {
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.FirebaseFirestore;
 
+import java.util.HashMap;
+import java.util.Map;
+
+public class RegisterActivity extends AppCompatActivity {
+    //button and text
     EditText ho;
     EditText ten;
     EditText address;
@@ -24,6 +30,10 @@ public class RegisterActivity extends AppCompatActivity {
     RadioGroup groupType;
     RadioButton typeUser;
     Button btn;
+    //firebase
+    private static final String TAG = "Cloud Firestore";
+    private static FirebaseFirestore db = FirebaseFirestore.getInstance();
+    Map<String, Object> user = new HashMap<String, Object>();
 
 
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -33,7 +43,7 @@ public class RegisterActivity extends AppCompatActivity {
         ten = (EditText) findViewById(R.id.lastname);
         address = (EditText) findViewById(R.id.address);
         username = (EditText) findViewById(R.id.username);
-        password = (EditText) findViewById(R.id.password);
+        password = (EditText) findViewById(R.id.login_password);
         confirm_password = (EditText) findViewById(R.id.re_password);
         groupType = (RadioGroup) findViewById(R.id.group_type);
         btn = (Button) findViewById(R.id.signup);
@@ -52,6 +62,29 @@ public class RegisterActivity extends AppCompatActivity {
                 String str_confirm_password = confirm_password.getText().toString();
                 String str_typeUser = typeUser.getText().toString();
                 //check data
+//                boolean duplicateUsername = true;
+//                db.collection("users")
+//                        .get()
+//                        .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+//                            public boolean temp;
+//                            @Override
+//                            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+//                                if (task.isSuccessful()) {
+//                                    for (QueryDocumentSnapshot document : task.getResult()) {
+//                                        if(str_username.equals(document.getId().toString())){
+//                                            temp = true;
+//                                            break;
+//                                        }
+//                                        Log.d(TAG, document.getId() + " => " + document.getData());
+//                                    }
+//
+//                                } else {
+//                                    Log.w(TAG, "Error getting documents.", task.getException());
+//                                }
+//                            }
+//                            duplicateUsername = temp;
+//                        });
+
                 if(TextUtils.isEmpty(str_ho) || TextUtils.isEmpty(str_ten)
                         || TextUtils.isEmpty(str_address) || TextUtils.isEmpty(str_username)
                         || TextUtils.isEmpty(str_password) || TextUtils.isEmpty(str_confirm_password)){
@@ -69,8 +102,27 @@ public class RegisterActivity extends AppCompatActivity {
                     System.out.println(str_confirm_password);
                     System.out.println(str_typeUser);
 
-                    Intent intent = new Intent(getApplicationContext(), MainBuyerHomepageActivity.class);
+                    //write on firebase
+                    user.put("firstname", str_ho);
+                    user.put("lastname", str_ten);
+                    user.put("address", str_address);
+                    user.put("username", str_username);
+                    user.put("password", str_password);
+                    user.put("role", str_typeUser);
+
+                    db.collection("users")
+                            .document(str_username)
+                            .set(user)
+                            .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                @Override
+                                public void onSuccess(Void unused) {
+
+                                }
+                            });
+
+                    Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
                     startActivity(intent);
+                    Toast.makeText(getApplicationContext(), "Tạo tài khoản thành công", Toast.LENGTH_SHORT).show();
                 }
             }
         });
